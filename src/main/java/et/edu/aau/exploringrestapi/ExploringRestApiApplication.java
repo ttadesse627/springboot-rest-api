403b9ece797f56a85d3c9f7cfd633eda4303806e
Tesfaye <ttadesse627@gmail.com>
2026-03-12 08:11:34 +0300

    Created REST API exploration project
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/et/edu/aau/exploringrestapi/ExploringRestApiApplication.java
@@ -0,0 +1,14 @@
+package et.edu.aau.exploringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringRestApiApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ExploringRestApiApplication.class, args);
+    }
+
+}
---
+++ src/main/java/et/edu/aau/exploringrestapi/HelloController.java
@@ -0,0 +1,18 @@
+package et.edu.aau.exploringrestapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() {
+        return "Ciao!";
+    }
+}
