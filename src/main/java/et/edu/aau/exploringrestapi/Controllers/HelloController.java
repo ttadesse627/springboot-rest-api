5dabd1a2a2d795a17930ba9ae4dfde5feaa4148c
Tesfaye <ttadesse627@gmail.com>
2026-03-13 21:32:25 +0300

    Separated by packages

--- src/main/java/et/edu/aau/exploringrestapi/Controllers/HelloController.java
+++ src/main/java/et/edu/aau/exploringrestapi/Controllers/HelloController.java
@@ -1,4 +1,4 @@
-package et.edu.aau.exploringrestapi;
+package et.edu.aau.exploringrestapi.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
